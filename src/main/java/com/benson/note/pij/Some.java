9b022387cfde5c45d5e6073dd5059230e5c23bef
zxpbenson <zxpbenson@163.com>
2021-08-26 14:46:44 +0800

    增加JUnitTest示例

---
+++ src/main/java/com/benson/note/pij/Some.java
@@ -0,0 +1,14 @@
+package com.benson.note.pij;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Some {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Some.class);
+
+    public int calculate() {
+        LOG.info("execute cal and return val");
+        return 1;
+    }
+}
